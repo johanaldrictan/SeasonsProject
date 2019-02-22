1f5c0f38dc84512d05b48704d71cc2f66faee55c
Johan Tan <johanaldrictan@gmail.com>
2019-02-21 20:14:18 -0800

    Added tile selecting

---
+++ Assets/Johan/CommanderController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CommanderController : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit2D hit;
+    HoverState hover_state;
+    Vector3Int lastTileLoc;
+
+    public Vector3Int tilePointer;
+
+    public Tilemap tileSelecting;
+
+    public TileBase tileSelector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Start Tile highlighting code
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hit = Physics2D.Raycast(ray.origin, ray.direction, 100f);
+        if (hit)
+        {
+            hover_state = HoverState.HOVER;
+        }
+        else
+        {
+            hover_state = HoverState.NONE;
+        }
+
+        if (hover_state == HoverState.HOVER)
+        {
+            //Mouse is hovering
+            //Debug.Log(mapController.GridToMap(mapController.grid.WorldToCell(hit.point)));
+            //Debug.Log(MapController.instance.grid.CellToWorld(MapController.instance.grid.WorldToCell(hit.point)));
+            if (lastTileLoc != null)
+                tileSelecting.SetTile(lastTileLoc, null);
+            lastTileLoc = MapController.instance.grid.WorldToCell(hit.point);
+            tileSelecting.SetTile(MapController.instance.grid.WorldToCell(hit.point), tileSelector);
+            tilePointer = MapController.instance.grid.WorldToCell(hit.point);
+        }
+        else
+        {
+            tileSelecting.SetTile(lastTileLoc, null);
+        }
+        //End Tile highlighting code
+    }
+}
+public enum HoverState
+{
+    NONE,
+    HOVER
+}
---
+++ Assets/MapController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapController : MonoBehaviour
+{
+    public static MapController instance;
+    public Grid grid;
+    public Tilemap plantableTiles;
+
+    //2D grid. value is the weight of the tile
+    public int[,] map;
+
+    public int mapWidth;
+    public int mapHeight;
+
+    private void Awake()
+    {
+        //each scene load I want a new instance of the mapcontroller but it needs to stay static
+        //needs to load start again in each scene
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+}
