2fcb8e0e5d8a39a01e326e20c6df3e8e1fef4b8a
Johan Tan <johanaldrictan@gmail.com>
2019-02-25 19:55:18 -0800

    Added pooling scripts

---
+++ Assets/Johan/PoolByBoundary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolByBoundary : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ////check if bullet
+        //Bullet b = collision.gameObject.GetComponent<Bullet>();
+        //if (b != null)
+        //{
+        //    //pool to player
+        //    b.gameObject.SetActive(false);
+        //}
+    }
+}
