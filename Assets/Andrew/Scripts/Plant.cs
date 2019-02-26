db7c16c0d593a8d05c22eb5f6648822784398bc5
dillonhl <44040206+dillonhl@users.noreply.github.com>
2019-02-25 21:31:39 -0800

    Plant should be able to shoot downwards
    Also added a pellet prefab for shooting

--- Assets/Andrew/Scripts/Plant.cs
+++ Assets/Andrew/Scripts/Plant.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Plant : MonoBehaviour
 {
+    public GameObject pelletPrefab;
+
+    private Collider2D plantCollider;
+    private Rigidbody2D plantRigidbody;
+
     private int Health = 10;            //has 10 health
     private int Damage = 1;             //does 1 damage per pellet
     private float TimeToLive = 60f;     //lives for 60 seconds
 
     private float shootTickValue = 0f;  //value for tracking time between shots
     private float shootTickRate = 1f;
+
+    void Start()
+    {
+        plantRigidbody = GetComponent<Rigidbody2D>();
+        plantCollider = GetComponent<Collider2D>();
+
+    }
 
     void Update()
     {
@@ -25,6 +37,9 @@ public class Plant : MonoBehaviour
 
     void Shoot()
     {
-        //instatiate a bullet at the plant's current location
+        GameObject pellet = Instantiate(pelletPrefab, this.transform.position, Quaternion.identity);
+        pellet.GetComponent<Rigidbody2D>().velocity = Vector2.down * 7;
+        Physics2D.IgnoreCollision(plantCollider, pellet.GetComponent<Collider2D>());
+    
     }
 }
