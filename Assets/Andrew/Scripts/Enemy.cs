8fa29b2f5b2569e24f2fe2f0cc3c887bc8f6bb31
Johan Tan <johanaldrictan@gmail.com>
2019-02-26 18:15:45 -0800

    fixed merge conflicts FIXED PROJECT VERSION FOR THE SECOND TIME

--- Assets/Andrew/Scripts/Enemy.cs
+++ Assets/Andrew/Scripts/Enemy.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField]private int Health = 5;
     [SerializeField]private int Damage = 1;
-=======
-    [SerializeField] private int Health = 5;
-    [SerializeField] private int Damage = 1;
->>>>>>> ScriptCombinationTest1
     [SerializeField] private int WalkSpeed = 1;
 
     public Plant plant;
@@ -23,41 +18,12 @@ public class Enemy : MonoBehaviour
 
     void Move()
     {
-<<<<<<< HEAD
-        this.transform.Translate(new Vector2(0, WalkSpeed * Time.deltaTime));
-=======
         this.transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
->>>>>>> ScriptCombinationTest1
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-<<<<<<< HEAD
-        if (collider.tag == "pellet")
-        {
-            //got hit, reduce health by plant damage
-            Health = Health - plant.GetDmg();
-            if (Health == 0)
-            {
-                Destroy(this.gameObject); 
-            }
-            
-        }
-
-        if(collider.tag == "town")
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    public int GetDmg()
-    {
-        return Damage;
-    }
-
-    
-}
-=======
+        
         if (collider.CompareTag("pellet"))
         {
             //got hit, reduce health by plant damage
@@ -74,5 +40,8 @@ public class Enemy : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+    public int GetDmg()
+    {
+        return Damage;
+    }
 }
->>>>>>> ScriptCombinationTest1
