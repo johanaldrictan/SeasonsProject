10c90521ae3f1655f21ac3c8de095407e5e52097
andrewabenoja <aabenoja@uci.edu>
2019-02-26 18:01:47 -0800

    merp
    merp

--- Assets/Andrew/Scripts/Enemy.cs
+++ Assets/Andrew/Scripts/Enemy.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private int Health = 5;
-    private int Damage = 1;
-    private int WalkSpeed = 1;
+    [SerializeField] private int Health = 5;
+    [SerializeField] private int Damage = 1;
+    [SerializeField] private int WalkSpeed = 1;
+
+    public Plant plant;
 
     void Update()
     {
         //move position of enemy based on WalkSpeed
+        Move();
+    }
+
+    void Move()
+    {
+        this.transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("pellet"))
+        {
+            //got hit, reduce health by plant damage
+            Health--;
+            if (Health == 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+
+        if (collider.CompareTag("town"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Assets/Andrew/Scripts/EnemySpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPlant;
+
+    public static EnemySpawner instance { get; private set; }
+
+    private float Timer;
+    private int SpawnPos;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private int[,] SpawnLocations = { { 0, 0 }, { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } };
+
+    void Start()
+    {
+        Timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MapController.instance.grid.CellToWorld()
+        Timer += Time.deltaTime;
+        if (Timer >= 5f)
+        {
+            SpawnPos = Random.Range(0, 5);
+            
+            if (SpawnPos == 0)
+            {
+                GameObject newEnemy = Instantiate(enemyPlant, new Vector3(-2, 0, 0), Quaternion.identity);
+                newEnemy.GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
+            }
+            else if (SpawnPos == 1)
+            {
+                GameObject newEnemy = Instantiate(enemyPlant, new Vector3(-1, 0, 0), Quaternion.identity);
+                newEnemy.GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
+            }
+            else if (SpawnPos == 2)
+            {
+                GameObject newEnemy = Instantiate(enemyPlant, new Vector3(0, 0, 0), Quaternion.identity);
+                newEnemy.GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
+            }
+            else if (SpawnPos == 3)
+            {
+                GameObject newEnemy = Instantiate(enemyPlant, new Vector3(1, 0, 0), Quaternion.identity);
+                newEnemy.GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
+            }
+            else if (SpawnPos == 4)
+            {
+                GameObject newEnemy = Instantiate(enemyPlant, new Vector3(2, 0, 0), Quaternion.identity);
+                newEnemy.GetComponent<Rigidbody2D>().velocity = Vector2.left * 1;
+            }
+
+            Timer = 0f;
+        }
+    }
+
+    /*IEnumerator SpawnEnemy()
+    {
+        while ()
+        yield return new WaitForSeconds((float)10);
+    }*/
+}
